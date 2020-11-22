222ba589fa7030ec56905cb67305bd5caa67b7fd
17002090220吕时海 <33956313+q1171874514@users.noreply.github.com>
2020-11-22 22:36:49 +0800

    背包算法

--- src/com/company/graph/Backpack.java
+++ src/com/company/graph/Backpack.java
@@ -6,63 +6,95 @@ package com.company.graph;
 public class Backpack {
     /**
      * 01背包
-     * @param volume      背包容量
+     * @param capacity    背包容量
      * @param num         物品数量
      * @param weight      物品重量
      * @param value       物品价值
      * @return
      */
-    public Double ZeroOneBackpack(Integer volume, Integer num, Integer[] weight, Double[] value) {
-        Double[] sum = new Double[volume + 1];
-        for (int i = 0; i < volume + 1; i++)
+    public Double zeroOneBackpack(Integer capacity, Integer num, Integer[] weight, Double[] value) {
+        Double[] sum = new Double[capacity + 1];
+        for (int i = 0; i < capacity + 1; i++)
             sum[i] = 0d;
         for (int i = 0; i < num; i ++)
-            for (int j = volume; j >=weight[i]; j--)
-                sum[j] = Math.max(sum[j], sum[j - weight[i]] + value[j]);
+            for (int j = capacity; j >=weight[i]; j--)
+                sum[j] = Math.max(sum[j], sum[j - weight[i]] + value[i]);
 
-        return sum[volume];
+        return sum[capacity];
     }
+
 
     /**
      * 完全背包
-     * @param volume      背包容量
+     * @param capacity    背包容量
      * @param num         物品数量
      * @param weight      物品重量
      * @param value       物品价值
      * @return
      */
-    public Double completeBackpack(Integer volume, Integer num, Integer[] weight, Double[] value) {
-        Double[] sum = new Double[volume + 1];
-        for (int i = 0; i < volume + 1; i++)
+    public Double completeBackpack(Integer capacity, Integer num, Integer[] weight, Double[] value) {
+        Double[] sum = new Double[capacity + 1];
+        for (int i = 0; i < capacity + 1; i++)
             sum[i] = 0d;
         for (int i = 0; i < num; i ++)
-            for (int j = weight[i]; j <= volume; j--)
-                sum[j] = Math.max(sum[j], sum[j - weight[i]] + value[j]);
+            for (int j = weight[i]; j <= capacity; j--)
+                sum[j] = Math.max(sum[j], sum[j - weight[i]] + value[i]);
 
-        return sum[volume];
+        return sum[capacity];
     }
 
     /**
      * 多重背包
-     * @param volume      背包容量
+     * @param capacity      背包容量
      * @param num         物品数量
      * @param singleNum   单个物品数量
      * @param weight      物品重量
      * @param value       物品价值
      * @return
      */
-    public Double multipleBackpack(Integer volume, Integer num, Integer[] singleNum, Integer[] weight, Double[] value) {
-        Double[] sum = new Double[volume + 1];
-        for (int i = 0; i < volume + 1; i++)
+    public Double multipleBackpack(Integer capacity, Integer num, Integer[] singleNum, Integer[] weight, Double[] value) {
+        Double[] sum = new Double[capacity + 1];
+        for (int i = 0; i < capacity + 1; i++)
             sum[i] = 0d;
 
         for (int i = 0; i < num; i ++)
             for (int k = 1; k <= singleNum[i]; k++)
-                for (int j = weight[i]; j <= volume; j--)
-                    sum[j] = Math.max(sum[j], sum[j - weight[i]] + value[j]);
+                for (int j = weight[i]; j <= capacity; j--)
+                    sum[j] = Math.max(sum[j], sum[j - weight[i]] + value[i]);
 
-        return sum[volume];
+        return sum[capacity];
     }
 
+    /**
+     * 二维01背包
+     * @param capacity    背包容量
+     * @param withinVolume      背包容积
+     * @param num         物品数量
+     * @param weight      物品重量
+     * @param volume      物品体积
+     * @param value       物品价值
+     * @return
+     */
+    public Double towDimZeroOneBackpack(Integer capacity, Integer withinVolume, Integer num,
+                                  Integer[] weight, Integer[] volume, Double[] value) {
+        Double[][] sum = new Double[capacity + 1][withinVolume + 1];
+        for (int i = 0; i < capacity + 1; i++)
+            for (int j = 0; j < withinVolume; j++)
+                sum[i][j] = 0d;
+        for (int i = 0; i < num; i ++)
+            for (int j = capacity; j >=weight[i]; j--)
+                for (int k = withinVolume; k >= volume[i]; k--)
+                    sum[j][k] = Math.max(sum[j][k], sum[j - weight[i]][k - volume[i]] + value[i]);
 
+        return sum[capacity][withinVolume];
+    }
+
+    public static void main(String[] args) {
+        Integer capacity = 20, withinVolume = 15, num = 5;
+        Integer weight[] = {6, 4, 5, 6, 5};
+        Integer volume[] = {4, 3, 4, 5, 3};
+        Double value[] = {7d, 5d, 6d, 2d, 3d};
+        Backpack backpack = new Backpack();
+        System.out.println(backpack.zeroOneBackpack(capacity, num, weight, value));
+    }
 }
