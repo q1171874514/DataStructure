735fc167ca929ab8b71bb873f33d6eb52d6e0ba3
17002090220吕时海 <33956313+q1171874514@users.noreply.github.com>
2020-11-22 21:30:58 +0800

    背包算法

--- src/com/company/graph/Backpack.java
+++ src/com/company/graph/Backpack.java
@@ -16,11 +16,10 @@ public class Backpack {
         Double[] sum = new Double[volume + 1];
         for (int i = 0; i < volume + 1; i++)
             sum[i] = 0d;
-        for (int i = 0; i < num; i ++) {
-            for (int j = volume; j >=weight[i]; j--) {
+        for (int i = 0; i < num; i ++)
+            for (int j = volume; j >=weight[i]; j--)
                 sum[j] = Math.max(sum[j], sum[j - weight[i]] + value[j]);
-            }
-        }
+
         return sum[volume];
     }
 
@@ -36,11 +35,34 @@ public class Backpack {
         Double[] sum = new Double[volume + 1];
         for (int i = 0; i < volume + 1; i++)
             sum[i] = 0d;
-        for (int i = 0; i < num; i ++) {
-            for (int j = weight[i]; j <= volume; j--) {
+        for (int i = 0; i < num; i ++)
+            for (int j = weight[i]; j <= volume; j--)
                 sum[j] = Math.max(sum[j], sum[j - weight[i]] + value[j]);
-            }
-        }
+
         return sum[volume];
     }
+
+    /**
+     * 多重背包
+     * @param volume      背包容量
+     * @param num         物品数量
+     * @param singleNum   单个物品数量
+     * @param weight      物品重量
+     * @param value       物品价值
+     * @return
+     */
+    public Double multipleBackpack(Integer volume, Integer num, Integer[] singleNum, Integer[] weight, Double[] value) {
+        Double[] sum = new Double[volume + 1];
+        for (int i = 0; i < volume + 1; i++)
+            sum[i] = 0d;
+
+        for (int i = 0; i < num; i ++)
+            for (int k = 1; k <= singleNum[i]; k++)
+                for (int j = weight[i]; j <= volume; j--)
+                    sum[j] = Math.max(sum[j], sum[j - weight[i]] + value[j]);
+
+        return sum[volume];
+    }
+
+
 }
